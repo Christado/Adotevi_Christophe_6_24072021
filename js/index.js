3fe0db86e54b8620872fb2881ccfd38c968d6cee
christado <adotevic@gmail.com>
2021-08-14 11:09:05 +0200

    description dynamique js page d'accueil

--- js/index.js
+++ js/index.js
@@ -19,6 +19,22 @@ function affichage(photographe){
     
     const lien = lienPhotographe(photographe);
     fichePhotographe.appendChild(lien);
+    const lieux = document.createElement("p"); 
+    lieux.classList.add("localisation");
+    lieux.innerText = photographe.city + ", " + photographe.country;
+    fichePhotographe.appendChild(lieux);
+    
+    const descrip1 = document.createElement("p"); 
+    descrip1.innerText = photographe.tagline;
+    fichePhotographe.appendChild(descrip1);
+    
+    const tarifs = document.createElement("p"); 
+    tarifs.classList.add("tarif");
+    tarifs.innerText = photographe.price + " € / jour ";
+    fichePhotographe.appendChild(tarifs);
+    const tags = tagPhotographe(photographe);
+    fichePhotographe.appendChild(tags);
+    
     main.appendChild(fichePhotographe);
 }
 
@@ -29,6 +45,28 @@ function lienPhotographe(photographe) {
     const img = document.createElement("img");
     img.src = "images/photographers/" + photographe.portrait;
     lien.appendChild(img);
+    
+    const nom = document.createElement("h2"); 
+    nom.innerText = photographe.name;
+    lien.appendChild(nom);
 
-    return lien;
+    return lien ;   
+}
+
+function tagPhotographe(photographe) {
+    const tag1 = document.createElement("nav"); 
+    const tag2 = document.createElement("ul");
+    const tags = photographe.tags;
+
+    for (let index = 0; index < tags.length; index++) {
+        const tag = tags[index];
+        const tagli = document.createElement("li"); 
+        tagli.innerText = "#" + tag;
+        tag2.appendChild(tagli);
+        
+    }
+
+    tag1.appendChild(tag2);
+
+    return tag1;
 }
